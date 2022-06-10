e393efeebd8559ee7a14dd5a28a723861814516b
Yurii <ukucher@gmail.com>
2022-06-11 00:06:19 +0300

    2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,40 +1,40 @@
 import Player from '@vimeo/player';
-
+var throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
-
 const player = new Player(iframe);
+const trottledOnPlayDuration = throttle(onPlayDuration, 1000);
 
 try {
-    const startTime = localStorage.getItem("videoplayer-current-time");
+  const startTime = localStorage.getItem('videoplayer-current-time');
 
-    player.setCurrentTime(startTime).then(function(seconds) {
-        // seconds = the actual time that the player seeked to
-    }).catch(function(error) {
-        switch (error.name) {
-            case 'RangeError':
-                // the time was less than 0 or greater than the video’s duration
-                break;
-    
-            default:
-                // some other error occurred
-                break;
-        }
+  player
+    .setCurrentTime(startTime)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
     });
+} catch (error) {
+  console.error('Set state error: ', error.message);
+}
 
+player.on('timeupdate', trottledOnPlayDuration);
+
+function onPlayDuration(evt) {
+  try {
+    let videoTimePosition = String(evt.seconds);
+    localStorage.setItem('videoplayer-current-time', videoTimePosition);
+    console.log(videoTimePosition);
   } catch (error) {
-    console.error("Set state error: ", error.message);
+    console.error('Set state error: ', error.message);
   }
-
-    player.on('timeupdate', onPlayDuration);
-
-    function onPlayDuration(evt) {
-        try {
-            let videoTimePosition = String(evt.seconds);
-            localStorage.setItem("videoplayer-current-time", videoTimePosition);
-          } catch (error) {
-            console.error("Set state error: ", error.message);
-          }
-
-
-    }
-
+}
